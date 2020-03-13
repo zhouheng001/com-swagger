74155f8e918afd3c298dbb645d35da349d442196
zhouheng <heng.zhou@nextentertain.com>
2020-03-13 20:51:03 +0800

    先解决访问报404

--- src/main/java/com/xiaominfo/swagger/config/SwaggerConfiguration.java
+++ src/main/java/com/xiaominfo/swagger/config/SwaggerConfiguration.java
@@ -1,15 +1,19 @@
 
 package com.xiaominfo.swagger.config;
 
+import com.fasterxml.classmate.TypeResolver;
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import com.google.common.collect.Lists;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -18,6 +22,7 @@ import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -26,14 +31,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
 @Import(BeanValidatorPluginsConfiguration.class)
 @EnableWebMvc
 public class SwaggerConfiguration {
 
+
     @Bean(value = "defaultApi")
     public Docket defaultApi() {
+
         ParameterBuilder parameterBuilder=new ParameterBuilder();
         List<Parameter> parameters= Lists.newArrayList();
         parameterBuilder.name("token").description("token令牌").modelRef(new ModelRef("String"))
@@ -55,6 +64,7 @@ public class SwaggerConfiguration {
 
     @Bean(value = "activityApi")
     public Docket activityApi() {
+
         ParameterBuilder parameterBuilder=new ParameterBuilder();
         List<Parameter> parameters= Lists.newArrayList();
         parameterBuilder.name("token").description("token令牌").modelRef(new ModelRef("String"))
